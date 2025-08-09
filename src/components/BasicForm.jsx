762af94851cce88ca45b59199104d21f22a10aa8
adolfo <aldomc99@gmail.com>
2025-08-09 14:17:56 -0600

    Esquema de Validación con yup

--- src/components/BasicForm.jsx
+++ src/components/BasicForm.jsx
@@ -1,7 +1,26 @@
+import { yupResolver } from '@hookform/resolvers/yup'
 import { useForm } from 'react-hook-form'
+import * as yup from 'yup'
 
+const schema = yup.object({
+  username: yup
+    .string()
+    .required('El Nombre es requerido'),
+  password: yup
+    .string()
+    .min(6, 'La contraseña debe tener al menos 6 caracteres')
+    .required('La contraseña es requerida'),
+  confirmPassword: yup
+    .string()
+    .oneOf([yup.ref('password'), null], 'Las contraseñas deben ser iguales')
+    .required('Confirma la contraseña')
+})
 export const BasicForm = () => {
-  const { register, handleSubmit } = useForm()
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid }
+  } = useForm()
 
   const onSubmit = (data) => {
     console.log(data)
@@ -11,7 +30,7 @@ export const BasicForm = () => {
     <form onSubmit={handleSubmit(onSubmit)}>
       <input type='text' {...register('username')} placeholder='Usuario' />
       <input type='password' {...register('password')} placeholder='Password' />
-      <input type='email' {...register('email')} placeholder='Email' />
+      <input type='password' {...register('confirmPassword')} placeholder='Confirmar Password' />
       <button type='submit'>Enviar</button>
     </form>
   )
