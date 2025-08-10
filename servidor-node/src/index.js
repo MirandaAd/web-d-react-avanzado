6cd4d9e55bd513922e8c28b42336275a901502f9
adolfo <aldomc99@gmail.com>
2025-08-10 11:31:47 -0600

    Servidor con node.js

---
+++ servidor-node/src/index.js
@@ -0,0 +1,11 @@
+const http = require('http')
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' })
+  res.end('Hola mundo desde Node.js!')
+})
+
+const PORT = 3000
+server.listen(PORT, () => {
+  console.log('Servidor en ejecución en http://localhost:3000')
+})
